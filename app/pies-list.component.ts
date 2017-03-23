091c82a5c58e14cce2f98cbe3c134e2155335b0a
Gathua Kiragu <kiragugathua@gmail.com>
2017-03-23 11:57:12 +0300

    Initial Commit Header Welcome To Moringa

---
+++ app/pies-list.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'pies',
+	template:`
+	<h1>Hello World. Welcome To Moringa school</h1>
+	`
+	
+})
+
+export class PiesListComponent{
+
+
+}
